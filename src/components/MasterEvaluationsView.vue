9c2142a8f9363942bb7ea1e12dbd1fd081ed6bb1
jiaoyuxuan <jiaoyuxuan@meiyuxiuxiu.com>
2016-02-20 14:35:17 +0800

    大师鉴宝列表页修改
    Change-Id: I433ba07bdfb12ff66878616b2de4cc0c0812c35b

--- src/components/MasterEvaluationsView.vue
+++ src/components/MasterEvaluationsView.vue
@@ -1,7 +1,7 @@
 <style lang="sass">
 @import '../styles/partials/var';
 .master-evaluations-view {
-    .share-bottom {
+    .bottom-button {
         z-index: 990;
         position: fixed;
         bottom: 0;
@@ -10,16 +10,24 @@
         background-image: url('#{$qn}/share/bottom.png');
         background-size: contain;
         font-size: 32px;
+        a {
+            display: block;
+        }
+    }
+    .bottom {
+        width: 100%;
+        height: 98px;
     }
 }
 </style>
 <template>
 <div class="master-evaluations-view">
     <evaluation-list :items="arr"></evaluation-list>
-    <div class="share-bottom flex bg-red white" v-link="{name: 'master-request', params: {id: this.self.id}}">
-        <div class="flex-1 center bold">申请鉴宝</div>
+    <div class="bottom-button flex bg-red white">
+        <a href="http://wx.meiyuxiuxiu.com/eval-add.html" class="flex-1 center">申请鉴宝</a>
     </div>
     <partial name="load-more" v-if="items.hasMore"></partial>
+    <div class="bottom"></div>
 </div>
 </template>
 <script>
