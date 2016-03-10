3e0f92880fccec666bbe92f0c4a864fddabda389
jiaoyuxuan <jiaoyuxuan@meiyuxiuxiu.com>
2016-03-10 20:03:23 +0800

    1.(修改)新版个人中心修改； 2.(修改)新版个人主页晒宝信息流；
    Change-Id: I1d0d75d0a6e38b184fc356018569cdfc0e29d86a

--- src/components/HomePageStory.vue
+++ src/components/HomePageStory.vue
@@ -1,11 +1,72 @@
 <style lang="sass">
 .homepage-story {
-    height: 100%;
+    .item {
+        padding: 0 16px 32px 16px;
+    }
+    .header {
+        padding: 20px 4px;
+        .name {
+            color: #666666;
+        }
+    }
+    .content {
+        .cover {
+            width: 100%;
+            padding-top: 100%;
+            background-size: cover;
+            background-position: center;
+        }
+        .title {
+            color: #666666;
+            text-align: center;
+            margin: 0 auto;
+            padding: 60px 0;
+            width: 480px;
+            line-height: 42px;
+            font-family: 'song-simple';
+        }
+        .social {
+            color: #d9d9d9;
+            text-align: center;
+            padding-bottom: 36px;
+            > div {
+                display: inline-block;
+                height: 28px;
+                line-height: 28px;
+            }
+            .left {
+                padding-right: 36px;
+            }
+            .right {
+                padding-left: 36px;
+            }
+        }
+    }
+    .empty-page {
+        height: 700px;
+    }
 }
 </style>
 <template>
-<div class="homepage-story bg-white">
-    <story-list :items="items"></story-list>
+<div class="homepage-story bg-default">
+    <div v-for="item in items" track-by="id" v-link="{name: 'story', params: {id: item.post_id}}" class="item">
+        <div class="header flex">
+            <div class="flex-1 flex">
+                <div v-if="$route.name === 'user-profile'" class="avatar-50" v-bg.sm="item.user.photo"></div>
+                <div v-else class="avatar-50" v-link="item.user | profile" v-bg.sm="item.user.photo"></div>
+                <div class="name margin-left font-26">{{item.user.name}}</div>
+            </div>
+            <div class="moment font-22 light">{{item.create_at | moment}}</div>
+        </div>
+        <div class="content bg-white">
+            <div class="cover" v-bg="item.cover" :class="{'video': item.cover_type==='video'}"></div>
+            <div class="title font-30 user-input" v-if="item.content">{{item.content}}</div>
+            <div class="social font-26">
+                <div class="left border-right"><i class="icon-like"></i><span>{{item.like}}</span></div>
+                <div class="right"><i class="icon-comment"></i><span>{{item.comment}}</span></div>
+            </div>
+        </div>
+    </div>
     <partial name="load-more" v-if="items.hasMore"></partial>
     <partial v-if="items.isEmpty" name="empty-page"></partial>
 </div>
--- src/components/ProfileView.vue
+++ src/components/ProfileView.vue
@@ -1,18 +1,18 @@
 <template>
 <div class="profile-view bg-default">
     <div class="separator"></div>
-    <div class="account bg-white border-bottom flex" @click="action('profile')">
+    <div class="account bg-white border-bottom flex" v-link="{name: 'user-profile', params:{id: userId}}">
         <div class="avatar-120" v-bg.sm="photo"></div>
         <div class="user flex-1">
             <p class="font-30">{{nickname}}</p>
             <p class="font-26 gray">{{role | role}}</p>
         </div>
-        <span class="icon-enter font-26 gray flex"></span>
+        <span class="font-26 red flex">进入个人主页<i class="icon-enter gray margin-left"></i></span>
     </div>
     <div class="community bg-white flex">
-        <div v-link="{name: 'user-story', params: {id: userId}}" class="border-right">
-            <p class="font-30" align="center">{{topic_count}}</p>
-            <p class="font-26 gray" align="center">晒宝</p>
+        <div v-link="{name: 'user-like', params:{id: userId}}" class="border-right">
+            <p class="font-30" align="center">{{like_count || 0}}</p>
+            <p class="font-26 gray" align="center">我的赞</p>
         </div>
         <div v-link="{name: 'following', params: {id: userId }}" class="border-right">
             <p class="font-30" align="center">{{follow_count}}</p>
@@ -25,14 +25,22 @@
     </div>
     <div class="separator-40"></div>
     <div class="rows">
+        <div class="row bg-white font-30 border-bottom" @click="action('profile')">
+            <span class="red icon-edit"></span>
+            <span class="">编辑资料</span>
+            <span class="icon-enter gray font-26"></span>
+        </div>
+    </div>
+    <div class="separator-40"></div>
+    <div class="rows">
         <div class="row bg-white font-30 border-bottom" v-link="{name: 'user-evaluation', params: {id: userId}}">
             <span class="red icon-eval"></span>
             <span class="">我的鉴宝</span>
             <span class="icon-enter gray font-26"></span>
         </div>
-        <div class="row bg-white font-30" v-link="{name: 'user-like', params:{id: userId}}">
-            <span class="icon-like red"></span>
-            <span>我的赞</span>
+        <div class="row bg-white font-30" v-link="{name: 'user-story', params: {id: userId}}">
+            <span class="icon-story red"></span>
+            <span>我的晒宝</span>
             <span class="icon-enter gray font-26"></span>
         </div>
     </div>
