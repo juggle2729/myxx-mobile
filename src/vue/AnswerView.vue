470b773aa7c28ae0bd797ce2ed0e0fd566e7e700
yuchangchun <yuchangchun@meiyuxiuxiu.com>
2016-12-05 11:17:53 +0800

    回答详情改版
    Change-Id: I8b348152fe7a600bdf1606f1c78bfce651ed15f1

--- src/vue/AnswerView.vue
+++ src/vue/AnswerView.vue
@@ -32,23 +32,16 @@
 .answer-view(v-if="!$loadingRouteData")
     header.bg-white
         .user.flex
-            avatar(:user="question.user")
-            .mgl.flex-1
-                .fz-30 {{question.user.name}}
-                .mgt-14.fz-22.light {{question.click_count}}人浏览
+            .flex-1.title.fz-34.bold.pdv-24.user-txt {{{question.description | input}}}
             .flex.red.fz-26.bdl.pdl-32.pdv-12(@click="gotoDownload")
                 icon(name="plus")
                 span 关注问题
-        .title.fz-34.bold.pdv-24.user-txt {{{question.description | input}}}
-        .fz-30.gray.pdb-26.user-txt(v-if="question.remark") {{{question.remark | input}}}
     .pictures.pdh-32.bg-white.scrollable(v-if="question.pictures.length")
         .pic(v-for="pic in question.pictures", v-bg.sm="pic", @click="coverflow(question.pictures, $index)")
-    template(v-if='question.categories.length')
-        topics(:topics="question.categories", :title="false")
     .bg-white.pdv-36
         share-button(txt="下载美玉秀秀，发表你的观点")
     .hr
-    
+
     .result.bg-white
         header
             .user.flex
@@ -64,7 +57,7 @@
             icon-like(:active='result.liked', :count='result.like_count', :target="result.id", type="jd")
             icon-comment.bdl(:count="result.comment_count", :id="result.id", type="jd")
         .hr
-    
+
     general-suggestion
 </template>
 <script>
@@ -96,13 +89,11 @@ export default {
 
     route: {
         data({to}) {
-            return this.$fetch(`sns/jianbao/${to.query.qid}`)
-                    .then(question => {
-                        this.question = question
-                        this.result = _.filter(this.question.results, (item) => {
-                            return item.id == this.$route.params.id
-                        })[0]
-                        this.setShareData({title: question.description, master: this.result.identifier.nickname, icon: question.pictures[0]})
+            return this.$fetch(`sns/results/${to.params.id}`)
+                    .then(result => {
+                        this.result = result
+                        this.question = result.jianbao
+                        this.setShareData({title: this.question.description, master: this.result.identifier.nickname, icon: this.question.pictures[0]})
                     })
         }
     }
