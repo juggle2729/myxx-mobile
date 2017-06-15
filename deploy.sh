9314e00561333bc0a36f56d3651506a41d89cdf2
czujun <chunzujun@meiyuxiuxiu.com>
2017-06-15 19:49:10 +0800

    [deploy.sh] 支持空目录拷贝
    Change-Id: I314beeaa6b80af81390edfd39502b6f0e9f16edf

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ then
     scp -r build/* front@120.27.194.242:/home/front/myxx-app
 else
     echo "====== DEPLOY WWW ====="
-    cp -r build/* ../www/static/mobile
+    mkdir -p ../www/static/mobile && cp -r build/* ../www/static/mobile
     scp -r build/* front@120.26.113.13:/home/front/www/static/mobile
 # scp -r build/* front@114.55.72.65:/home/front/apps/www/static/mobile
 fi
