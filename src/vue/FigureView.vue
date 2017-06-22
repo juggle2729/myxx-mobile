c0408ed513e47872914c6e29bd39aabfd790f5d6
yuchangchun <yuchangchun@meiyuxiuxiu.com>
2017-06-22 09:39:43 +0800

    [improve]人物志采用750图，添加标题展示 [bugfix]line-clamp在特定安卓版本上会文本顶部显示不全，调整其行高来修复
    Change-Id: I66fe6cbccd5a1e91b0be47f7dbb5e91da4fa7bb6

--- src/vue/FigureView.vue
+++ src/vue/FigureView.vue
@@ -1,5 +1,8 @@
 <style lang="stylus">
 .figure-view
+    .title
+        padding 60px 32px 20px
+        line-height 1.5
     img.cover
         width 100%
     .media
@@ -22,9 +25,10 @@
 </style>
 <template lang="pug">
 .figure-view
-    img.cover(:src="config.img + cover")
+    img.cover(v-if="cover", :src="config.img + cover + '_750'")
+    .title.fz-40.bold {{title}}
     .media(v-for="m in medias")
-        img(v-if="m.media_type === 'picture'", :src="config.img + m.media")
+        img(v-if="m.media_type === 'picture'", :src="config.img + m.media + '_750'")
         .video.bg(v-if="m.media_type === 'video'", v-bg.video='m.media', @click.stop="play(m.media)")
         p(v-if="m.media_type === 'text'") {{m.media}}
     us(v-if="profile", :entry="profile")
@@ -43,6 +47,7 @@ export default {
 
     data() {
         return {
+            title: '',
             cover: '',
             medias: [],
             profile: undefined,
@@ -53,6 +58,7 @@ export default {
     route: {
         data({to}) {
             return this.$fetch(`sites/${to.params.userId}/articles/${to.params.articleId}`).then(article => {
+                    this.title = article.title
                     this.cover = article.image
                     this.medias = article.medias
                     return this.$fetch(`users/${to.params.userId}/profile`)
