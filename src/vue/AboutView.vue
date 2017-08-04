14393ece4f02a3271072fcfc27bb88f11c6ffebd
ggzhang <zhanggengen@meiyuxiuxiu.com>
2017-08-04 17:05:28 +0800

    [3.9修改关于美玉秀秀页面]
    Change-Id: I5944d1379e7aba3c0c7f1b002b5d5509b7423f6e

--- src/vue/AboutView.vue
+++ src/vue/AboutView.vue
@@ -1,47 +1,61 @@
 <style lang="stylus">
 .about-view
     .imgs
-        padding-top: 162px
-        padding-bottom: 113px
+        padding-top 120px
         > img
-            width: 178px
-            height: 178px
-        .default
-            margin-top: 36px
-        .gray
-            margin-top: 20px
-    .text
-        padding: 0 92px
-        > p
-            margin-top: 36px
-            line-height: 56px
+            width 216px
+            height 244px
+        .text
+            p:nth-child(1)
+                margin-top 56px
+            p:nth-child(3)
+                margin-top 72px
+    .help
+        margin: 60px 32px
+        .icon-enter
+            height: 22px
+            width: 22px
 </style>
 <template  lang="pug">
-.about-view
+.about-view.bg-light-gray
     .imgs.center
-        img(:src="'profile/about.png' | qn")
-        p.fz-34.default 美玉秀秀
-        p.fz-22.gray v{{version}}
+        img(:src="'profile/logo.png' | qn")
+        p.fz-26.gray.mgt-36 v{{version}}
         .text
-            p.fz-30 美玉秀秀是国内最大的和田玉线上交流App，高品质和田玉资源集合地。
-            p.fz-30 美玉秀秀移动端是为全国各地的和田玉爱好者、藏家、商家、相关领域的大师以及权威人士提供专业和田玉鉴定、社交、交易的综合性平台。
+            p.fz-40 最大的和田玉爱好者平台
+            p.mgt-20.fz-30 玩玉最专业 买玉最放心
             p.fz-30 客服电话：
                 a.red(href="tel:4000587266") 400-0587-266
+    .help.bg-white
+        .line-height-100.fz-30.bold.mgl-32.pdr-32.gray 帮助中心
+        .line-height-100.flex.fz-26.bdt.mgl-32.pdr-32(v-link="{path: '/help/mall'}")
+            .flex-1 商城交易常见问题
+            icon.gray(name="enter")
+        .line-height-100.flex.fz-26.bdt.mgl-32.pdr-32(v-for='item in problems', v-link="{path: '/help/desc/' + item.type, params: {type: item.type}}")
+            .flex-1 {{item.title}}
+            icon.gray(name="enter")
 </template>
 <script>
+import help from '../help'
 export default {
     name: 'about-view',
 
     data() {
         return {
             version: '1.0',
-            cookie: document.cookie
+            problems: []
         }
     },
 
     ready() {
         this.action('version')
             .then(v => this.version = v)
+
+        _.forIn(help.desc, (value, key) => {
+            if(/^about_/.test(key)) {
+                this.problems.push(value)
+            }
+        })
     }
 }
 </script>
