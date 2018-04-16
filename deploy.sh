2242de07d6d0de94b2ebb75870a30eb2ab152ee7
shjliang <liangshunjie@meiyuxiuxiu.com>
2018-04-16 20:24:53 +0800

    修改文件编码为UNIX,增加部署环境的判断
    Change-Id: I5f0c5c54d7215a05d6158c9a3bf385b2df4d1c4c

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 if [ "$1" = "app" ]
 then
-    echo "====== DEPLOY APP ====="
-    scp -r build/* front@120.26.113.13:/home/front/app
-    scp -r build/* front@120.27.194.242:/home/front/myxx-app
+    if [ "$2" = "prod" ]
+    then
+        echo "====== DEPLOY APP TO PROD ====="
+        scp -r build/* front@120.27.194.242:/home/front/myxx-app
+    else
+        echo "====== DEPLOY APP TO TEST ====="
+        scp -r build/* front@120.26.113.13:/home/front/app
+    fi
 else
     echo "====== DEPLOY WWW ====="
     mkdir -p ../www/static/mobile && rm -rf ../www/static/mobile/* && cp -r build/* ../www/static/mobile
     scp -r build/* front@120.26.113.13:/home/front/www/static/mobile
-    scp -r build/* front@114.55.72.65:/home/front/apps/www/static/mobile
+    #scp -r build/* front@114.55.72.65:/home/front/apps/www/static/mobile
 fi
 echo "====== DEPLOY END ====="
