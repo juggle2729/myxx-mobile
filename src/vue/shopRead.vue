79bc27ee6f1fb7014e85446891e266eeab3a119d
qjdai <daiqinjun@meiyuxiuxiu.com>
2018-05-23 11:41:30 +0800

    [开店必读文案修改]
    Change-Id: Ia0ed5383e2d3b94bcc97a32b69b5c2c3ac74abe3

--- src/vue/shopRead.vue
+++ src/vue/shopRead.vue
@@ -1,17 +1,33 @@
 <template>
-    <div class="page  pdl-36 pdr-26">
-        <div class="content black_47 fz-30 pdt-50">
-            <p>· 开通30天内新店铺专享，助力开单</p>
-            整体提升店铺商品曝光，最适合新店铺的推广方式一个店铺只有1次购买机会整体提升店铺商品曝光，
-            最适合新店铺的推广方式一个店铺只有1次购买机会整体提升店铺商品曝光，
-            最适合新店铺的推广方式一个店铺只有1次购买机会整体提升店铺商品曝光，
-            最适合新店铺的推广方式一个店铺只有1次购买机会整体提升店铺商品曝光，
-            最适合新店铺的推广方式一个店铺只有1次购买机会整体提升店铺商品曝光，
-            最适合新店铺的推广方式一个店铺只有1次购买机会整体提升店铺商品曝光，
-            最适合新店铺的推广方式一个店铺只有1次购买机会整体提升店铺商品曝光，
-            最适合新店铺的推广方式一个店铺只有1次购买机会整体提升店铺商品曝光，
-            最适合新店铺的推广方式一个店铺只有1次购买机会整体提升店铺商品曝光，
-            最适合新店铺的推广方式一个店铺只有有长期1次购买机会。
+    <div class="page">
+        <div class="content black_47 fz-30 pdt-50 pdl-36 pdr-26">
+           <ul>
+               <li>
+                   <div class="fz-30 bold">入驻要求</div>
+                   <div>1.商家须遵守平台“三天无理由退货”的规定，不得无故拒绝退货申请。</div>
+                   <div>2.商品必须保证货真价实，如实描述，不得欺骗消费者</div>
+                   <div>3.不得使用违规手段刷单、刷评价，严重违反者关闭店铺。</div>
+                   <div>4.重视并尊重平台用户，及时回复私信并文明待人。</div>
+               </li>
+               <li>
+                   <div class="fz-30 bold">交易贴士</div>
+                   <div>1.商家需在24小时内发货，如有异常应与买家主动联系邴谢尚发货时间。查看平台交易规则>></div>
+                   <div>2.买家申请退货后，买家只用承担退货运费，无需承担商家发货运费。查看平台退货规则>></div>
+                   <div>3.买家付款后，钱款将进入由银行监管的第三方账户，交易成功后商家方可提现。查看平台提现规则>></div>
+                   <div>4.商家开通这个权限就不会漏掉买家私信了。查看并进行设置>></div>
+                   <div>5.商家这样发布商品将获得更好的曝光和推广。查规范看平台商品发布规范>></div>
+               </li>
+               <li>
+                   <div class="fz-30 bold">收费规则</div>
+                   <div>1.开店0费用</div>
+                   <div>2.通过直播或专场拍卖产生的订单平台收取6%的技术服务费。<a href="https://cms.meiyuxiuxiu.com/2018/03/04/charge/">了解详情>></a></div>
+                   <div>2..除直播或专场拍卖产生的订单之外的所有订单征收0.6%的支付通道费，这部分费用由微信和支付宝收取。<a href="https://cms.meiyuxiuxiu.com/2018/03/04/charge/">了解详情>></a></div>
+               </li>
+               <li>
+                   <div class="fz-30 bold">开店入驻流程</div>
+                   <div>注册——填写开店信息——初审通过——上传商品（至少20件）——复审通过——成功入驻</div>
+               </li>
+           </ul>
         </div>
         <div class="button">
             <div class="disagree fz-30 txt-center" @click="back">不同意</div>
@@ -57,18 +73,29 @@ export default {
 <style lang="stylus">
     .page
         width: 100%
-        height: 100%
         background-color #F7F9FC
+        box-sizing border-box
+        padding-bottom 128px
         .content
             line-height 1.8
-            font-family PingFang SC
-            p
-                line-height 1.8
+            font-family "微软雅黑"
+            li
+                margin-bottom 20px
+                div
+                    line-height 1.6
+                    a
+                        color blue
+
         .button
             position fixed
-            bottom 40px
+            width 100%
+            height 136px
+            bottom 0
             display -webkit-box
             -webkit-box-pack center
+            padding-top 28px
+            background #f7f9fc
+            border-top 1px solid #e8eaed
             .disagree
                 width: 190px
                 height: 80px
