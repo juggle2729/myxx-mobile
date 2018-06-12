9e125053ea6bd981fed8fb367da043d13ddb6719
qjdai <daiqinjun@meiyuxiuxiu.com>
2018-06-12 19:01:15 +0800

    [fix bug]
    Change-Id: Iadbfcd98a364222078f7414a116e94808f7f1138

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const UA = navigator.userAgent
 
 let [api, img, video, download] = ((env='test') => {
     env = /meiyuxiuxiu\.com$/.test(location.hostname) ? 'prod' : (/meiyuxiuxiu\.net$/.test(location.hostname) ? 'test' : env)
-    IS_APP = /myxx/i.test(UA)
+    const IS_APP = /myxx/i.test(UA)
     const hosts = {
         api: {
             test: '//test.api.meiyuxiuxiu.net',
